9455d43860cfd48b697e06ad54d6ddc4d5d7e096
alafi <ahmadlafi@gmail.com>
2018-05-28 19:54:01 +0300

    Sourcing MengeCore dlls directly from Menge directory

--- MengeCS/MengeWrapper.cs
+++ MengeCS/MengeWrapper.cs
@@ -8,44 +8,50 @@ namespace MengeCS
 {
     class MengeWrapper
     {
-        [DllImport("MengeCore")]
+#if DEBUG
+        const string MENGE_DLL = "MengeCore_d";
+#else
+        const string MENGE_DLL = "MengeCore";
+#endif
+
+        [DllImport(MENGE_DLL)]
         public static extern bool InitSimulator([MarshalAs(UnmanagedType.LPStr)] String behaveFile,
                                                 [MarshalAs(UnmanagedType.LPStr)] String sceneFile,
                                                 [MarshalAs(UnmanagedType.LPStr)] String model,
                                                 [MarshalAs(UnmanagedType.LPStr)] String pluginPath
                                                 );
-        
-        [DllImport("MengeCore")]
+
+        [DllImport(MENGE_DLL)]
         public static extern float SetTimeStep(float timestep);
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern bool DoStep();
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern uint AgentCount();
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern bool GetAgentPosition(uint i, ref float x, ref float y, ref float z);
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern bool GetAgentVelocity(uint i, ref float x, ref float y, ref float z);
 
-        [DllImport("MengeCore")]
-        public static extern bool GetAgentOrient(uint i, ref float x, ref float y );
+        [DllImport(MENGE_DLL)]
+        public static extern bool GetAgentOrient(uint i, ref float x, ref float y);
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern int GetAgentClass(uint i);
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern float GetAgentRadius(uint i);
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern int ExternalTriggerCount();
 
-        [DllImport("MengeCore")]
+        [DllImport(MENGE_DLL)]
         public static extern IntPtr ExternalTriggerName(int i);
 
-        [DllImport("MengeCore", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MENGE_DLL, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
         public static extern void FireExternalTrigger([MarshalAs(UnmanagedType.LPStr)] string lpString);
     }
 }
